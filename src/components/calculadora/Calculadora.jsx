530d42039489bdcfe952bc6463097bc22394af10
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-08-23 09:52:11 -0300

    Ajustes

--- src/components/calculadora/Calculadora.jsx
+++ src/components/calculadora/Calculadora.jsx
@@ -5,7 +5,7 @@ export default function Calculadora(props) {
     return (
         <section className="formulario">
           <label>Altura: </label>
-          <input type="text" name="faltura" value={props.altura} placeHolder="Ex. 1.76m²" onChange={(e) => props.handleChangeAltura(e)}/>
+          <input type="text" name="faltura" value={props.altura} placeHolder="Ex. 1.76m" onChange={(e) => props.handleChangeAltura(e)}/>
           <label>Peso: </label>
           <input type="text" name="fpeso" value={props.peso} placeHolder="Ex. 85Kg" onChange={(e) => props.handleChangePeso(e)}/>
           <button onClick={() => props.imcCalc()}>Calcular</button>
